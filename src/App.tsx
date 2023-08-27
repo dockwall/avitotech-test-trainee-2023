d314c7f5e27d0bd807f5470ac131dcd64b838717
Stas <leshuksu@gmail.com>
2023-08-27 22:13:32 +0300

    added GamePage mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import GamePage from "./pages/GamePage";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/game/:id" element={<GamePage />} />
       </Routes>
     </BrowserRouter>
   );
