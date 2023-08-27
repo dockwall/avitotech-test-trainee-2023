f8d341e4d3f62c25e5aa68363fc14484c6325ee7
Stas <leshuksu@gmail.com>
2023-08-27 23:51:00 +0300

    used store in App, added logic to work with it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGames } from "../store/gameSlice";
+import { RootState, AppDispatch } from "../store/store";
 
 const Home: React.FC = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const { games, loading, error } = useSelector(
+    (state: RootState) => state.games
+  );
+  useEffect(() => {
+    dispatch(fetchGames()); // TODO: add filters and sorters
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
       <h1>place for List of Games</h1>
+      <p>Games in list: {games.length}</p>
     </div>
   );
 };
