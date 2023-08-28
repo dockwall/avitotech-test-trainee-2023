f31f5c86d2009bb8bc5aab0275215666ac6c4734
Stas <leshuksu@gmail.com>
2023-08-29 02:00:22 +0300

    added types for filter params

--- src/types/gameTypes.ts
+++ src/types/gameTypes.ts
@@ -31,3 +31,58 @@ export interface MinimumSystemRequirements {
   graphics: string;
   storage: string;
 }
+
+export interface GameFilterParams {
+  platform?: "pc" | "browser" | "all";
+  category?: GameTag;
+  tag?: GameTag[];
+  "sort-by"?: "release-date" | "popularity" | "alphabetical" | "relevance";
+  id?: number;
+}
+
+export type GameTag =
+  | "mmorpg"
+  | "shooter"
+  | "strategy"
+  | "moba"
+  | "racing"
+  | "sports"
+  | "social"
+  | "sandbox"
+  | "open-world"
+  | "survival"
+  | "pvp"
+  | "pve"
+  | "pixel"
+  | "voxel"
+  | "zombie"
+  | "turn-based"
+  | "first-person"
+  | "third-Person"
+  | "top-down"
+  | "tank"
+  | "space"
+  | "sailing"
+  | "side-scroller"
+  | "superhero"
+  | "permadeath"
+  | "card"
+  | "battle-royale"
+  | "mmo"
+  | "mmofps"
+  | "mmotps"
+  | "3d"
+  | "2d"
+  | "anime"
+  | "fantasy"
+  | "sci-fi"
+  | "fighting"
+  | "action-rpg"
+  | "action"
+  | "military"
+  | "martial-arts"
+  | "flight"
+  | "low-spec"
+  | "tower-defense"
+  | "horror"
+  | "mmorts";
