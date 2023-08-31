2f2c4e68edb6fdf5bec8e655506457c11395d3fc
Stas <leshuksu@gmail.com>
2023-08-31 23:17:53 +0300

    raw game page is done

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,10 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Card, Image, Button, Grid } from "semantic-ui-react";
+import { useParams, useNavigate } from "react-router-dom";
+import { fetchGameById } from "../store/gameSlice";
+import { RootState, AppDispatch } from "../store/store";
+import { MinimumSystemRequirements } from "../types/gameTypes";
+import LoadingIndicator from "../components/LoadingIndicator";
+
+const formatDate = (dateString: string) => {
+  return new Date(dateString).toLocaleDateString("ru-RU");
+};
+
+const getMinSystemReqs = (minSystemReqs?: MinimumSystemRequirements) => {
+  if (!minSystemReqs) {
+    return null;
+  }
+
+  const reqs = Object.entries(minSystemReqs).map(([key, text]) => (
+    <li key={key}>{text}</li>
+  ));
+
+  return (
+    <>
+      <strong>Системные требования:</strong>
+      <ul>{reqs}</ul>
+    </>
+  );
+};
 
 const GamePage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { gameDetails, loading, error } = useSelector(
+    (state: RootState) => state.games
+  );
+
+  useEffect(() => {
+    dispatch(fetchGameById(Number(id)));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div>
-      <h1>selected game test</h1>
-    </div>
+    <Container>
+      <Card fluid>
+        <Card.Content>
+          <Card.Header>{gameDetails?.title}</Card.Header>
+          <Card.Description>
+            <Grid columns={2}>
+              <Grid.Column>
+                <Image src={gameDetails?.thumbnail} alt={gameDetails?.title} />
+                <p>
+                  <strong>Дата релиза:</strong>{" "}
+                  {formatDate(gameDetails?.release_date || "")}
+                </p>
+                <p>
+                  <strong>Издатель:</strong> {gameDetails?.publisher}
+                </p>
+                <p>
+                  <strong>Разработчик:</strong> {gameDetails?.developer}
+                </p>
+              </Grid.Column>
+              <Grid.Column>
+                <p>
+                  <strong>Жанр:</strong> {gameDetails?.genre}
+                </p>
+                <p>
+                  {getMinSystemReqs(gameDetails?.minimum_system_requirements)}
+                </p>
+              </Grid.Column>
+            </Grid>
+          </Card.Description>
+          <Button onClick={() => navigate(-1)}>Вернуться к списку игр</Button>
+        </Card.Content>
+      </Card>
+    </Container>
   );
 };
 
